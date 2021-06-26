e1ba2156bacc9107c67a6b761653b87c40af02d7
David Kumar <david.dn.kumar@gmail.com>
2021-06-26 16:47:28 -0500

    update portfolio screen UI and styling

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, FlatList} from 'react-native';
+import styles from './styles';
+import PortfolioCoin from "../../components/PortfolioCoin";
+const image =  require('../../../assets/images/portfolio.jpg');
 
-const PortfolioScreen = (props) =>{
+const portfolioCoins = [{
+    id: '1',
+    name: 'Virtual Dollars',
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+    symbol: 'USD',
+    amount: 10,
+    valueUSD: 10000,
+}, {
+    id: '2',
+    name: 'Bitcoin',
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+    symbol: 'USD',
+    amount: 10,
+    valueUSD: 20000,
+}, {
+    id: '3',
+    name: 'Etherium',
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+    symbol: 'ETH',
+    amount: 10,
+    valueUSD: 30000,
+}]
 
+const PortfolioScreen = () => {
     return (
-        <View>
-            <Text>HomeScreen component</Text>
+        <View style={styles.root}>
+            <Image style={styles.image} source={image} />
+            <View style={styles.balanceContainer}>
+                <Text style={styles.label}>Portfolio balance</Text>
+                <Text style={styles.balance}>$100</Text>
+            </View>
+
+            <FlatList
+                style={{width: '100%'}}
+                data={portfolioCoins}
+                renderItem={({item}) => <PortfolioCoin portfolioCoin={item} />}
+            />
         </View>
     );
-
-}
+};
 
 export default PortfolioScreen;
