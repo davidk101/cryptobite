8159711615f7f1f4f85fea93397f1687bea6d654
David Kumar <david.dn.kumar@gmail.com>
2021-06-26 15:30:22 -0500

    add homescreen UI and styling

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import styles from './styles';
+const image = require('../../../assets/images/logo.png');
 
-const HomeScreen = (props) =>{
+const HomeScreen = () =>{
 
     return (
-        <View>
-            <Text>HomeScreen component</Text>
+        <View style = {styles.root}>
+            <Image style = {styles.image} source = {image} />
+            <Text style = {styles.header1}>Welcome to CryptoBite!</Text>
+            <Text style = {styles.header2}>Use blockchain hashing algorithms to mine CryptoBite and make real-time investments in the crypto market!</Text>
         </View>
     );
 
