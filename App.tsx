8fc9b93710a0b8e41e5a2c888f456411f92a9186
David Kumar <david.dn.kumar@gmail.com>
2021-07-07 18:26:08 -0500

    debug aws-exports issue

--- App.tsx
+++ App.tsx
@@ -7,9 +7,8 @@ import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
 
-import Amplify from 'aws-amplify'
-// @ts-ignore
-import config from './aws-exports'
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 Amplify.configure(config)
 
 export default function App() {
---
+++ aws-exports.d.ts
@@ -0,0 +1,4 @@
+declare const awsmobile: {
+    "aws_project_region": "us-east-1"
+};
+export default awsmobile;
