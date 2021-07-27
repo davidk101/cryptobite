b18d3ea5367446229c7233e3e59f4fb2ccf8b931
David Kumar <david.dn.kumar@gmail.com>
2021-07-26 19:02:03 -0500

    update portfolioScreen functionality

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,48 +1,66 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {View, Text, Image, FlatList} from 'react-native';
 import styles from './styles';
+import {API, graphql, graphqlOperation} from 'aws-amplify'
+import {} from '../../graphql/queries'
 import PortfolioCoin from "../../components/PortfolioCoin";
+import AppContext from "../../utils/AppContext";
+import {getUserPortfolio} from "./queries";
 const image =  require('../../../assets/images/portfolio.png');
 
-const portfolioCoins = [{
-    id: '1',
-    name: 'Virtual Dollars',
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    symbol: 'USD',
-    amount: 10,
-    valueUSD: 10000,
-}, {
-    id: '2',
-    name: 'Bitcoin',
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    symbol: 'USD',
-    amount: 10,
-    valueUSD: 20000,
-}, {
-    id: '3',
-    name: 'Etherium',
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    symbol: 'ETH',
-    amount: 10,
-    valueUSD: 30000,
-}]
 
 const PortfolioScreen = () => {
+    const [balance, setBalance] = useState(0);
+    const [portfolioCoins, setPortfolioCoins] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const {userId} = useContext(AppContext);
+
+    const fetchPortfolio = async () => {
+        setLoading(true);
+        try {
+            const response = await API.graphql(
+                graphqlOperation(
+                    getUserPortfolio,
+                    {id: userId},
+                )
+            )
+            console.log(response.data.getUser);
+            setBalance(response.data.getUser.networth)
+            setPortfolioCoins(response.data.getUser.portfolioCoins.items)
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchPortfolio();
+    }, [])
+
     return (
         <View style={styles.root}>
-            <Image style={styles.image} source={image} />
-            <View style={styles.balanceContainer}>
-                <Text style={styles.label}>Portfolio balance</Text>
-                <Text style={styles.balance}>$100</Text>
-            </View>
-
             <FlatList
                 style={{width: '100%'}}
                 data={portfolioCoins}
-                renderItem={({item}) => <PortfolioCoin portfolioCoin={item} />}
+                renderItem={({item}) => <PortfolioCoin portfolioCoin={item}/>}
+                onRefresh={fetchPortfolio}
+                refreshing={loading}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponentStyle={{alignItems: 'center'}}
+                ListHeaderComponent={() => (
+                    <>
+                        <Image style={styles.image} source={image}/>
+                        <View style={styles.balanceContainer}>
+                            <Text style={styles.label}>Portfolio balance</Text>
+                            <Text style={styles.balance}>${balance}</Text>
+                        </View>
+                    </>
+                )}
             />
         </View>
-    );
-};
+    )
+}
 
 export default PortfolioScreen;
---
+++ src/screens/PortfolioScreen/queries.ts
@@ -0,0 +1,20 @@
+export const getUserPortfolio = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      networth
+      portfolioCoins {
+        items {
+          id
+          amount
+          coin{
+            id
+            name
+            symbol
+          }
+        }
+        nextToken
+      }
+    }
+  }
+`;
